0ece5064fff1f596fd7054da387888394153973b
esmeraldino-lk <lucas.esmeraldino.main@hotmail.com>
2025-03-12 12:12:46 -0300

    Initial clear registry content

---
+++ agent/clear-trend-registry.bat
@@ -0,0 +1,20 @@
+@echo off
+REG DELETE "HKLM\SOFTWARE\TrendMicro" /f
+REG DELETE "HKLM\SOFTWARE\WOW6432Node\TrendMicro" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\TmFilter" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\TmPreFilter" /f
+REG DELETE "HKLM\SYSTEM\CurrentControlSet\Services\tmcomm" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Google\Chrome\NativeMessagingHosts\com.trendmicro.chrome.dlp" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Edge\NativeMessagingHosts\com.trendmicro.edge.dlp" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\Trend Micro Endpoint Basecamp" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\Trend Micro Endpoint Basecamp" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\Trend Micro Web Service Communicator" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet002\Services\Trend Micro Endpoint Basecamp /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet002\Services\Trend Micro Web Service Communicator" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Trend Micro Endpoint Basecamp" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Trend Micro Web Service Communicator" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\TrendMicro\Deep Security Agent" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\TrendMicro\Deep Security Agent" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\EventLog\Application\Deep Security Agent" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet002\Services\EventLog\Application\Deep Security Agent" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\Deep Security Agent" /f
